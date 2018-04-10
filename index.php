204e1536cc1682f824413dac198f83a66ae02e03
王锐 <wangrui@bangbangbang.cn>
2018-04-10 18:07:25 +0800

    no message

--- index.php
+++ index.php
@@ -9,15 +9,8 @@ require 'vendor/autoload.php';
 $cof = require 'config/main.php'
 $app = new \Slim\App(['settings' => $cof]);
 $container = $app->getContainer();
-$container['logger'] = function($c) {
-    $logger = new \Monolog\Logger('my_logger');
-    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
-    $logger->pushHandler($file_handler);
-    return $logger;
-};
 
 $app->get('/', function (Request $req,  Response $res, $args = []) {
-	$this->logger->addInfo("Hello World!");
     return $res->withStatus(200)->write('Hello World!');
 });
 $app->run();
