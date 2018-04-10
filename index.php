9640fc20c3711734045fe377cf55fe92c4e8b7c4
王锐 <wangrui@bangbangbang.cn>
2018-04-10 18:10:17 +0800

    no message

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 
 $cof = require 'config/main.php'
-$app = new Slim\App(['settings' => $cof]);
+$app = new \Slim\App(['settings' => $cof]);
 
 $app->get('/', function (Request $req,  Response $res, $args = []) {
     return $res->withStatus(200)->write('Hello World!');
