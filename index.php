a5a7ba0d0efeed8c27f171c8939def75e0109460
王锐 <wangrui@bangbangbang.cn>
2018-04-10 17:17:27 +0800

    no message

--- index.php
+++ index.php
@@ -18,9 +18,9 @@ use buff\lib;
  * your Slim application now by passing an associative array
  * of setting names and values into the application constructor.
  */
-$app = new Slim\App();
-$app->view(new \JsonWrappe());
-$app->add(new \JsonMiddleware());
+$app = new \Slim\Slim();
+$app->view(new JsonWrappe());
+$app->add(new JsonMiddleware());
 /**
  * Step 3: Define the Slim application routes
  *
