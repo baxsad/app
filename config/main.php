4483c4f2b69cbe3114fb70b2d7cd8394cae31f1b
王锐 <wangrui@bangbangbang.cn>
2018-04-10 17:54:08 +0800

    no message

---
+++ config/main.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	//app
+	'displayErrorDetails'    => true,
+	'addContentLengthHeader' => false,
+
+	//db
+	'db' => array(
+		'host'   => '',
+		'user'   => '',
+		'pass'   => '',
+		'dbname' => '',
+	),
+);
--- index.php
+++ index.php
@@ -1,38 +1,14 @@
 <?php
-/**
- * Step 1: Require the Slim Framework using Composer's autoloader
- *
- * If you are not using Composer, you need to load Slim Framework with your own
- * PSR-4 autoloader.
- */
+
+
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+
 require 'vendor/autoload.php';
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
-$app = new \Slim\App();
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
- * is an anonymous function.
- */
+
+$cof = require 'config/main.php'
+$app = new \Slim\App(['settings' => $cof]);
 $app->get('/', function (Request $req,  Response $res, $args = []) {
     return $res->withStatus(200)->write('Hello World!');
 });
-
-/**
- * Step 4: Run the Slim application
- *
- * This method should be called last. This executes the Slim application
- * and returns the HTTP response to the HTTP client.
- */
 $app->run();
