fe8e1aa8e8a92d3677bce9b977bddfb15d4fed29
王锐 <wangrui@bangbangbang.cn>
2018-04-11 13:30:25 +0800

    no message

--- classes/slim/Middleware.php
+++ classes/slim/Middleware.php
@@ -6,17 +6,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 /**
  * Slim To enable CORS
  */
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
-});
+// $app->options('/{routes:.+}', function ($request, $response, $args) {
+//     return $response;
+// });
 
 /**
  * Slim Middleware
  */
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-});
+// $app->add(function ($req, $res, $next) {
+//     $response = $next($req, $res);
+//     return $response
+//         ->withHeader('Access-Control-Allow-Origin', '*')
+//         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+//         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+// });
