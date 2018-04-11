c8cdecc6f3297545f2db543c0842e7882f6bddc9
王锐 <wangrui@bangbangbang.cn>
2018-04-11 13:38:50 +0800

    no message

--- classes/handlers/NotFoundHandler.php
+++ classes/handlers/NotFoundHandler.php
@@ -15,9 +15,6 @@ class NotFoundHandler
     }
 
     public function __invoke(Request $request, Response $response) {
-        return $response
-            ->withStatus(404)
-            ->withHeader('Content-Type', 'text/html')
-            ->write('<center><h1 style="font-size: 10em">404</h1></center>');
+    	
     }
 }
