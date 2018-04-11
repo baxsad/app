c5e7b66e6b82c2e0f439b490f3ee26f7d51379ec
王锐 <wangrui@bangbangbang.cn>
2018-04-11 13:20:17 +0800

    no message

--- classes/handlers/NotFoundHandler.php
+++ classes/handlers/NotFoundHandler.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Container\ContainerInterface;
 
-class NotFoundHandlers 
+class NotFoundHandler
 {
     protected $container;
 
