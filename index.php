4374b0157deca39eeaa88cc98652cde56ff2e44f
王锐 <wangrui@bangbangbang.cn>
2018-04-11 12:37:37 +0800

    no message

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-require 'vendor/autoload.php';
-/**
- * Slim 
- */
-$cof = require 'config/main.php';
+
+// OW!~
+require __DIR__ . '/vendor/autoload.php';
+$cof = require __DIR__ . '/config/main.php';
 $app = new \Slim\App($cof);
+require __DIR__ . '/classes/app/Dependencies.php';
+require __DIR__ . '/classes/app/Middleware.php';
+require __DIR__ . '/classes/app/Routes.php';
 $app->run();
