5a2246ea839032c0e1fa58dacf30166e869f3c23
王锐 <wangrui@bangbangbang.cn>
2018-04-12 17:56:12 +0800

    no message

--- controllers/user/UserController.php
+++ controllers/user/UserController.php
@@ -31,7 +31,7 @@ class UserController
         } else {
           $start = microtime(true);
           $user = $this->table->orWhere([['uid', '=', $uid],['account', '=', $account]])->get()->first();
-          var_dump($this->table->where('uid', $uid)->orWhere('account', $account)->toSql());die;
+          var_dump($this->table->orWhere(['uid','account'], [$uid,$account])->toSql());die;
           $expend = (microtime(true)-$start)*1000;
           if (empty($user)) {
             $this->responseService->withFailure();
