e10e62056ec48972667098b3cd57419b3797b88a
王锐 <wangrui@bangbangbang.cn>
2018-04-12 19:00:31 +0800

    no message

--- classes/slim/Middleware.php
+++ classes/slim/Middleware.php
@@ -3,6 +3,8 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+// alexberce/Slim-API
+// tuupola/slim-api-skeleton
 /**
  * Slim To enable CORS
  */
@@ -22,3 +24,49 @@ $app->add(function ($req, $res, $next) {
 		->withHeader('Allow', 'GET, POST, PUT, DELETE, OPTIONS')
 		->withHeader('Content-Type', 'application/json; charset=UTF-8');
 });
+
+$app->add(
+	new \Slim\Middleware\JwtAuthentication(
+		[
+			"secret"    => Environment::$jwtSecretKey,
+			"relaxed" => Environment::$developmentDomains,
+			"algorithm" => "HS256",
+			"header" => "X-Token",
+			"rules"     => [
+				new \Slim\Middleware\JwtAuthentication\RequestPathRule(
+					[
+						"path"        => "/",
+						"passthrough" => ['/v1/token'],
+					]
+				),
+				new \Slim\Middleware\JwtAuthentication\RequestMethodRule(
+					[
+						"passthrough" => ["OPTIONS"],
+					]
+				),
+				new \Slim\Middleware\JwtAuthentication\RequestMethodRule(
+					[
+						"passthrough" => ["POST"],
+					    "path" => "/v1/users"
+					]
+				),
+				new \Slim\Middleware\JwtAuthentication\RequestMethodRule(
+					[
+						"passthrough" => ["POST"],
+						"path" => "/v1/account"
+					]
+				)
+			],
+			"callback"  => function ($request, $response, $arguments) use ($container) {
+				$container["jwt"] = $arguments["decoded"];
+			},
+			"error"     => function ($request, $response, $arguments) {
+				(new Invobox\Api\Response\ResponseService())
+					->withErrorMessage($arguments["message"])
+					->withStatusCode(401)
+					->withErrorCode(ResponseErrorCodes::RESPONSE_CODE_UNAUTHORIZED)
+					->write();
+			},
+		]
+	)
+);
