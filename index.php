17426240c357bd35b3fff6ae82ab28d3bdfa3064
王锐 <wangrui@bangbangbang.cn>
2018-04-12 13:45:49 +0800

    no message

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 date_default_timezone_set('PRC');
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/lib/Buff.php';
 
 // OW!~
+// Buff registry
+Buff::registry();
 // Instantiate the app
 $cof = require __DIR__ . '/config/main.php';
 $app = new \Slim\App(['settings' => $cof]);
-
 /**
  * Register dependencies
  * Register handlers
@@ -17,6 +19,5 @@ require __DIR__ . '/classes/slim/Dependencies.php';
 require __DIR__ . '/classes/slim/Handlers.php';
 require __DIR__ . '/classes/slim/Middleware.php';
 require __DIR__ . '/classes/slim/Routes.php';
-
 // run app
 $app->run();
