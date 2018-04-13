ee796affb170cb6d7a4122361d7e0fbec7ddf1d3
王锐 <wangrui@bangbangbang.cn>
2018-04-13 13:13:42 +0800

    no message

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-date_default_timezone_set('PRC');
+date_default_timezone_set('Asia/Shanghai');
+
+defined('SYS_ENV') or define('SYS_ENV', 'dev');
+defined('isMaintenance') or define('isMaintenance', false);
+
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/lib/APP.php';
 
