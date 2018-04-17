6edef86016a15eaf2e2b10913f635cad7775ab90
王锐 <wangrui@bangbangbang.cn>
2018-04-17 15:38:34 +0800

    no message

--- config/main.php
+++ config/main.php
@@ -21,7 +21,7 @@ return array(
 
 	    //logger
 	    'logger' => array(
-		    'name' => 'girleo',
+		    'name' => 'Buff',
             'level' => Monolog\Logger::DEBUG,
             'path' => __DIR__ . '/../logs/app.log',
 	    )
